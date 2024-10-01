1280fb9dce284a5be3cb65bb8a5922e1447c0dbf
Red Panda <Deep_Sea_Penguin@protonmail.com>
2024-09-30 20:51:03 -0400

    finishing up

--- index.mjs
+++ index.mjs
@@ -1,10 +1,32 @@
 // Importing database functions. DO NOT MODIFY THIS LINE.
-import { central, db1, db2, db3, vault } from "./databases.js";
+import { central, db1, db2, db3, vault } from "./databases.mjs";
 
-function getUserData(id) {
-  const dbs = {
-    db1: db1,
-    db2: db2,
-    db3: db3
-  };
+async function getUserData(id) {
+  try{
+    const dbs = {
+      db1: db1,
+      db2: db2,
+      db3: db3
+    };
+    let db = await central(id)
+    let mainData = await dbs[db](id)
+    let secretData = await vault(id)
+    let userObj = {
+      id: id,
+      name: secretData.name,
+      username: mainData.username,
+      email: secretData.email,
+      address: secretData.address,
+      phone: secretData.phone,
+      website: mainData.website,
+      company: mainData.company
+    }
+  console.log(userObj)
+  return userObj
+  } catch(err){
+    console.log(err)
+  }   
 }
+getUserData(7)
+
+
